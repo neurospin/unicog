ad363a81bc392b6e9f2e3bf60dfa5789721ef1f0
isadenghien <isabelle.denghien@cea.fr>
2015-11-03 15:11:56 +0100

    clean-up

--- build/lib.linux-x86_64-2.7/unicogfmri/__init__.py
+++
@@ -1,4 +0,0 @@
-#from localizer import *
-#from utils_unicog import *
-#
-#__all__ = ['localizer', 'utils_unicog']
--- build/lib.linux-x86_64-2.7/unicogfmri/localizer/__init__.py
+++
@@ -1,5 +0,0 @@
-#from importation_data import *
-#from utils_localizer import *
-#from volume_glm import *
-#
-#__all__ = ['importation_data', 'utils_localizer', 'volume_glm']
