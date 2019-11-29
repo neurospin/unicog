abb7083c22057d2aaaa80488a4d6f31ddcff3457
isadenghien <isabelle.denghien@cea.fr>
2019-11-29 14:33:39 +0100

    update for python3

--- utils_unicog/computing_resources/example/qsub_python.py
+++ utils_unicog/computing_resources/example/qsub_python.py
@@ -1,32 +1,32 @@
-#!/usr/bin/python
 # Example PBS cluster job submission in Python
- 
-from popen2 import popen2
-import time
-import os, sys, glob
- 
+import subprocess
+import os, glob
+
 # Loop over your jobs
 
 list_scripts = sorted(glob.glob("/home/user/files/file*.py"))
 
-for i in list_scripts:
-    # Open a pipe to the qsub command.
-    output, input = popen2('qsub')
-     
+for e, i in enumerate(list_scripts, 1):
     # Customize your options here
-    job_name = "test_run4_%s" % i
+    job_name = "test_run4_%s" % os.path.basename(i)
     walltime = "1:00:00"
     processors = "nodes=1:ppn=1"
     command = "python %s" % i
     job_string = """#!/bin/bash
     #PBS -N %s
-    #PBS -q Unicog_run4  
+    #PBS -q Unicog_run4 
     #PBS -l walltime=%s
     #PBS -l %s
-    cd "/home/user/output"
+    cd "/home/id983365/output"
     %s""" % (job_name, walltime, processors, command)
-     
+
+    # Create a pbs file
+    name_file = "qsub_cmd_%s" %e
+    fichier = open(name_file, "w")
+    fichier.write(job_string)
+    fichier.close()
+
     # Send job_string to qsub
-    input.write(job_string)
-    input.close()
+    cmd = "qsub %s"%(name_file)
+    subprocess.call(cmd, shell=True)
 
